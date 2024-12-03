5a6f0cf9970599109e46e0199c55b75f1374e9b9
Francis Jain <francisjain4@gmail.com>
2024-12-03 21:55:28 +0530

    adding statless into stateful and ysvsa and import from another page

---
+++ lib/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Home Screen'),
+            DisplayWidget(count: _count),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            _count++;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class DisplayWidget extends StatelessWidget {
+  const DisplayWidget({
+    super.key,
+    required int count,
+  }) : _count = count;
+
+  final int _count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('$_count');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,19 @@
+import 'package:brocode_tutorial_1/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   MainApp({super.key});
 
   @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  final _textController = TextEditingController();
-
-  String _searchTerm = '';
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Container(
-            decoration: BoxDecoration(color: Colors.grey[200]),
-            padding: EdgeInsets.all(20.0),
-            margin: EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                TextField(
-                    controller: _textController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Enter a search term',
-                    )),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _searchTerm = _textController.text;
-                      });
-                    },
-                    child: Text('Search')),
-                SizedBox(
-                  height: 10,
-                ),
-                Text('Search Term: $_searchTerm'),
-              ],
-            ),
-          ),
-        ),
-      ),
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home:  const HomeScreen(),
     );
   }
 }
